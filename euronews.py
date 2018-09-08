7a9298f44d7509225048c841a1a733eedbe901fd
David Bourgault <dav.bour@gmail.com>
2018-09-07 20:39:50 -0400

    Improved: better requests parallelization
    * Moved to requests_futures for simpler and better (?) parallelization
    + Added socks5 proxy support
    + Added player option

--- euronews.py
+++ euronews.py
@@ -2,10 +2,12 @@ import json
 import argparse
 import re
 import requests
+from requests_futures.sessions import FuturesSession
 import signal
 import sys
 import subprocess
 import concurrent.futures
+import threading
 
 from time import sleep
 
@@ -15,12 +17,16 @@ RE_TS_SEGMENT      = re.compile(r'.*-(?P<timestamp>\d+)-(?P<id>\d+)\.ts')
 
 def main(args):
     # Get server URL
+    proxies = {}
+    if args.socks5 is not None:
+        proxies = { 'http': args.socks5, 'https': args.socks5 }
+
     print("Requesting servers URLs")
-    r = requests.get("http://www.euronews.com/api/watchlive.json")
+    r = requests.get("http://www.euronews.com/api/watchlive.json", proxies=proxies)
     url = "http:" + json.loads(r.text)["url"]
 
     # Get video servers URLs
-    r = requests.get(url)
+    r = requests.get(url, proxies=proxies)
     data = json.loads(r.text)
 
     if (data["status"] == "ko"):
@@ -28,11 +34,11 @@ def main(args):
         exit(1)
 
     primary = data["primary"][:-13]
-    backup = data["backup"][:-13]
+    #backup = data["backup"][:-13] #TODO: Use backup stream if needed
 
     # Get available streams
     print("Requesting available streams")
-    r = requests.get(primary + "playlist.m3u8")
+    r = requests.get(primary + "playlist.m3u8", proxies=proxies)
     lines = r.text.split("\n")
 
     streams = {}
@@ -52,50 +58,59 @@ def main(args):
 
     # Download   
     def _sigint(sig, frame):
+        nonlocal done
         print("Received SIGINT (threads may take some time to close...)")
-        executor.shutdown(wait=False)
-        out.close()
-        exit(0)
+        done = True
 
-    def _download_segment(url, sid):
-        r = requests.get(url)
-        segments[sid] = r.content
-        #print("GET {}".format(sid))
+    def _player():
+        nonlocal done
+        ret = subprocess.call([args.player, "euronews.ts"])
+        print("Player closed with return code {}".format(ret))
+        done = True
 
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+    done = False    
+
+    session = FuturesSession(max_workers=4)
     signal.signal(signal.SIGINT, _sigint)
 
     out = open("euronews.ts","wb")
 
     segments = {}
     next_segment_id = 0
+    last_segment_id = 0
+    last_timestamp = 0
 
-    while (True):
+    while not done:
         # Get available segments
-        r = requests.get(stream)
+        r = requests.get(stream, proxies=proxies)
         lines = r.text.split("\n")
 
         # Extract TS file names (including timestamp and id)
         for line in lines:
             match = RE_TS_SEGMENT.match(line)
             if match is not None:
-                sid = int(match.group('id'))
-                #print("{}".format(sid))
+                timestamp = int(match.group('timestamp'))
 
-                if sid > next_segment_id and sid not in segments.keys():
-                    if (next_segment_id == 0):
-                        next_segment_id = sid
-                    segments[sid] = b''
-                    executor.submit(_download_segment, primary + line, sid)
+                if timestamp > last_timestamp:
+                    segments[last_segment_id] = session.get(primary + line, proxies=proxies)
+                    last_timestamp = timestamp
+                    last_segment_id += 1
+                    #TODO: too many indentation levels
 
         # Write all downloaded segments to TS file
-        while ((next_segment_id) in segments.keys() and segments[next_segment_id] != b''):
-            out.write(segments[next_segment_id])
-            #print("PUT {}".format(next_segment_id))
+        while ((next_segment_id) in segments.keys() and segments[next_segment_id].done()) and not done:
+            out.write(segments[next_segment_id].result().content)
             del segments[next_segment_id]
             next_segment_id += 1
+            if (next_segment_id == 3 and args.player is not None):
+                threading.Thread(target=_player).start()
+
+    session.close()
+    out.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-q","--quality", type=int, help="stream quality")
+    parser.add_argument("-q","--quality", type=int, help="stream quality, in vertical pixels")
+    parser.add_argument("-s","--socks5",  type=str, help="socks5 proxy")
+    parser.add_argument("-p","--player",  type=str, help="media player to launch")
     main(parser.parse_args())
